68bfda42885ca08d59c61866fd771eed88537f6d
Connor Lynch <empirepenguins@gmail.com>
2019-04-04 14:45:42 -0400

    added the button on the home menu that takes you to econestoga

--- app/src/main/java/com/example/assignmenttodolist/MainActivity.java
+++ app/src/main/java/com/example/assignmenttodolist/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.assignmenttodolist;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+
+import java.net.URI;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,5 +50,13 @@ public class MainActivity extends AppCompatActivity {
 
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void VisitPage(View view) {
+        String link = "https://conestoga.desire2learn.com/d2l/home/244302";
+        Uri viewURI = Uri.parse(link);
+        Intent viewIntent = new Intent(Intent.ACTION_VIEW, viewURI);
+        startActivity(viewIntent);
     }
 }
